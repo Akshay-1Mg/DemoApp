aa476fc219f986dc803a28ddb01904d6a11542da
Akshay-1Mg <akshay.kataria@1mg.com>
2020-07-21 15:51:42 +0530

    added unit test for a method

--- DemoAppTests/DemoAppTests.swift
+++ DemoAppTests/DemoAppTests.swift
@@ -10,25 +10,19 @@ import XCTest
 @testable import DemoApp
 
 class DemoAppTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  var sut: ViewController!
+  
+  override func setUp() {
+    super.setUp()
+    sut = ViewController()
+  }
+  
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
+  
+  func testCalculateSumMethod() {
+    XCTAssertEqual(sut.calculateTheSum(a: 5, b: 5), 10)
+  }
 }
