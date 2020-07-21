0d086b6dcb339bd135c2f99e543f79583c4e954f
Akshay-1Mg <akshay.kataria@1mg.com>
2020-07-21 15:26:45 +0530

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Akshay on 21/07/20.
+//  Copyright © 2020 1mg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
