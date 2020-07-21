55416a8d8601321508076780c0be532adbccb1c2
Akshay-1Mg <akshay.kataria@1mg.com>
2020-07-21 15:48:52 +0530

    added a public method

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  // MARK: - View lifecycle methods
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  // MARK: - Public methods
+  
+  func calculateTheSum(a: Int, b: Int) -> Int {
+    return a + b
+  }
+  
 }
 
